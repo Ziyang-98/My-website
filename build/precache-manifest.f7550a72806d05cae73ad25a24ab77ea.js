d41285d587a56ca5f18a2843269738456b3b0e00
Lim Zi Yang <limziyang8@gmail.com>
2020-12-06 20:12:00 +0800

    Add build

---
+++ build/precache-manifest.f7550a72806d05cae73ad25a24ab77ea.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aecca92320ca4172744aa80315031de4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b68791699af673991f4f",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "dcb1b66e9cf41febb757",
+    "url": "/static/js/2.c664ae96.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/static/js/2.c664ae96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b68791699af673991f4f",
+    "url": "/static/js/main.05b78e13.chunk.js"
+  },
+  {
+    "revision": "68a9665f6992de0c4239",
+    "url": "/static/js/runtime-main.fc75219d.js"
+  },
+  {
+    "revision": "04dcc3319a8290b8775f59d1a9fd170a",
+    "url": "/static/media/Java.04dcc331.png"
+  },
+  {
+    "revision": "5b84f71a39ef01173c7bd41530b4140d",
+    "url": "/static/media/avataaars.5b84f71a.png"
+  },
+  {
+    "revision": "4ff3b845e40b88d36d180e57be5a4c75",
+    "url": "/static/media/bob.4ff3b845.PNG"
+  },
+  {
+    "revision": "df91c7d73385c6749cf9e1076f3e816a",
+    "url": "/static/media/boblogo.df91c7d7.PNG"
+  },
+  {
+    "revision": "f278be7973516129af9e64d45a0c6f4d",
+    "url": "/static/media/c.f278be79.png"
+  },
+  {
+    "revision": "75c767fa096c1b951611e9963484c361",
+    "url": "/static/media/commoncents.75c767fa.png"
+  },
+  {
+    "revision": "88bf805c23b644ba63457c250f346b6c",
+    "url": "/static/media/commoncentslogo.88bf805c.png"
+  },
+  {
+    "revision": "3f8e68f49f7dba3390a43f8fcc1a14f7",
+    "url": "/static/media/css3.3f8e68f4.png"
+  },
+  {
+    "revision": "e2ec5b8ae151ce2f18a7e4c5de8e807f",
+    "url": "/static/media/dp1.e2ec5b8a.png"
+  },
+  {
+    "revision": "ee1369e96e441ad0119530d10264b8b4",
+    "url": "/static/media/git.ee1369e9.png"
+  },
+  {
+    "revision": "94b52b8c62fe6dcd9be4648119e4a0c6",
+    "url": "/static/media/gradle.94b52b8c.png"
+  },
+  {
+    "revision": "2ea01c0e92883b6e481dd2e62f5e6d4e",
+    "url": "/static/media/html5.2ea01c0e.png"
+  },
+  {
+    "revision": "01e3ddb34cb7b5723ba61a7b2608f804",
+    "url": "/static/media/javafx.01e3ddb3.PNG"
+  },
+  {
+    "revision": "874cef545e6a61bf90dc82fa8368c3da",
+    "url": "/static/media/modplanner.874cef54.png"
+  },
+  {
+    "revision": "2ad2bafa57551e9b676f53447b7c2e3b",
+    "url": "/static/media/mongodb.2ad2bafa.PNG"
+  },
+  {
+    "revision": "069c411a86dc28b64f256e1bfa5a5cb1",
+    "url": "/static/media/nodejs.069c411a.png"
+  },
+  {
+    "revision": "cb75cc0083b5ccedbb4541df0ea89439",
+    "url": "/static/media/ruby.cb75cc00.png"
+  }
+]);
